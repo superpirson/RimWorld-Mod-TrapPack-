e22a3ef6da203df584667c604167fb799648a9f6
superpirson <alexpatton@alexpattonsmbp.att.net>
2014-08-06 18:23:06 -0500

    recked all the classes

--- Source/TrapPack/ShieldMod/ThingAddons.cs
+++ Source/TrapPack/ShieldMod/ThingAddons.cs
@@ -13,7 +13,7 @@ using System.Collections.Generic;
 
 public class AnimatedThingDef : ThingDef{
 	public List<ThingAddons.Frame> frames;
-	public bool loop_around = true;
+	public bool play = true;
 }
 
 
@@ -23,9 +23,21 @@ namespace ThingAddons
 		public Frame(Material material){
 			this.material = material;
 		}
-		
+		public Frame(){
+		}
 		public Material material = Verse.BaseContent.BadMat;
-		public string tex_name = "UNAMED FRAME!";
+		private string tex_name_intenal = "err, bad tex string";
+		public string tex_name {
+			get {
+				return tex_name_intenal;
+			}
+			set{
+				tex_name_intenal = value;
+				material = new Material(VerseBase.MatBases.Cutout);
+				material.mainTexture = ContentFinder<Texture2D>.Get (tex_name_intenal, true);
+ 			
+			}
+		}
 		public bool play_through = true;
 		public int frame_number = 0;
 		public int frame_delay = 0;
@@ -43,9 +55,8 @@ namespace ThingAddons
 					this.next_frame_index = 0;
 				}
 				
-			}
+			}	
 				return frames[this.next_frame_index];
-		
 		}
 	
 	}
@@ -55,8 +66,6 @@ namespace ThingAddons
 		//animations are drawn on top of the thing's textures.
 		private int tick_count = 0;
 		protected AnimatedThingDef animated_thing_def;
-		
-		public List<Frame> frames;
 		public Frame current_frame;
 		
 		//this is how long the animatior should wait before starting to cycle
@@ -66,7 +75,8 @@ namespace ThingAddons
 			
 			this.animated_thing_def = (AnimatedThingDef)this.def;
 			
-			if (frames == null){
+			if (this.animated_thing_def.frames == null || this.animated_thing_def.frames.Count <= 0){
+				this.animated_thing_def.frames = new List<Frame>();
 			if (this.def.folderDrawMats == null || this.def.folderDrawMats.Count <= 0)
 			{
 				Log.Error("Animated thing tried to find texture folder, but it was empty/null!");
@@ -75,22 +85,21 @@ namespace ThingAddons
 			else if (this.def.folderDrawMats.Count == 1)
 			{
 				Log.Warning("Animated thing tried to find texture folder, but found only one texture.");
-					this.frames.Add(new Frame(this.def.folderDrawMats[0]));
+					this.animated_thing_def.frames.Add(new Frame(this.def.folderDrawMats[0]));
+					this.current_frame = this.animated_thing_def.frames[0];
 			}
 			else{
 					foreach (Material mat in this.def.folderDrawMats){
-						this.frames.Add(new Frame(mat));
+						this.animated_thing_def.frames.Add(new Frame(mat));
 					}
 			}
 			}
-			else{
-			this.frames = animated_thing_def.frames;
-			}
-			this.current_frame = frames[0];
+			this.current_frame = this.animated_thing_def.frames[0];
+	
 			base.SpawnSetup();
 		}	
 		public override void Tick(){
-			if (!this.current_frame.play_through){base.Tick();return;}
+	
 			if (wait_ticks > 0){
 				wait_ticks--;
 				base.Tick();
@@ -101,9 +110,11 @@ namespace ThingAddons
 			}
 			tick_count = 0;
 			
-			current_frame = current_frame.get_next_frame(frames);
 			this.def.drawMat = current_frame.material;
 			Find.MapDrawer.MapChanged(this.Position, MapChangeType.Things);
+			if (!this.animated_thing_def.play){base.Tick();return;}
+
+			current_frame = current_frame.get_next_frame(animated_thing_def.frames);
 			base.Tick ();
 		}
 		public override void Draw ()
@@ -112,30 +123,79 @@ namespace ThingAddons
 			this.Comps_Draw ();
 			base.Draw ();
 			//Log.Message("draw was called!");
-		}	
-
-		public override void PrintOnto(SectionLayer layer)
-		{
-			base.PrintOnto(layer);
 		}
-
 		public override Material DrawMat (IntRot rot)
 		{
-			//by default, this function does not get called!
-			//Log.Message("thingaddon's drawmat was called!");
-			if (this.frames == null || this.frames.Count <= 0)
-			{
-				return this.def.DrawMat (rot);
-			}
-		
-			// ---- the game gets the textures in the texture folder in alphabetical order, I recomend nameing your textures thing1, thing2, thing3 if there are many frames
 			return this.current_frame.material;
 		}
 	}
 
 	public class AnimatedBuilding : Building{
 		//this is an exact copy of animatedThing, but a it extends buildings
-//*/
-
-}
-}
+		//animations are drawn on top of the thing's textures.
+		//animations are drawn on top of the thing's textures.
+		private int tick_count = 0;
+		protected AnimatedThingDef animated_thing_def;
+		public Frame current_frame;
+		
+		//this is how long the animatior should wait before starting to cycle
+		public int wait_ticks = 0;
+		
+		public override void SpawnSetup(){
+			
+			this.animated_thing_def = (AnimatedThingDef)this.def;
+			
+			if (this.animated_thing_def.frames == null || this.animated_thing_def.frames.Count <= 0){
+				this.animated_thing_def.frames = new List<Frame>();
+				if (this.def.folderDrawMats == null || this.def.folderDrawMats.Count <= 0)
+				{
+					Log.Error("Animated thing tried to find texture folder, but it was empty/null!");
+					return;
+				}
+				else if (this.def.folderDrawMats.Count == 1)
+				{
+					Log.Warning("Animated thing tried to find texture folder, but found only one texture.");
+					this.animated_thing_def.frames.Add(new Frame(this.def.folderDrawMats[0]));
+					this.current_frame = this.animated_thing_def.frames[0];
+				}
+				else{
+					foreach (Material mat in this.def.folderDrawMats){
+						this.animated_thing_def.frames.Add(new Frame(mat));
+					}
+				}
+			}
+			this.current_frame = this.animated_thing_def.frames[0];
+			base.SpawnSetup();
+		}	
+		public override void Tick(){
+			
+			if (wait_ticks > 0){
+				wait_ticks--;
+				base.Tick();
+				return;
+			}
+			if (tick_count++ < current_frame.frame_delay){
+				base.Tick (); return;
+			}
+			tick_count = 0;
+			
+			this.def.drawMat = current_frame.material;
+			Find.MapDrawer.MapChanged(this.Position, MapChangeType.Things);
+			if (!this.animated_thing_def.play){base.Tick();return;}
+			
+			current_frame = current_frame.get_next_frame(animated_thing_def.frames);
+			base.Tick ();
+		}
+		public override void Draw ()
+		{
+			//	Log.Message("thingaddon's Draw was called!");	
+			this.Comps_Draw ();
+			base.Draw ();
+			//Log.Message("draw was called!");
+		}
+		public override Material DrawMat (IntRot rot)
+		{
+			return this.current_frame.material;
+		}
+	}
+	}
